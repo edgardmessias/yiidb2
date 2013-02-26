431c68dd15900f513d9a00cd1797aee62d1d49b8
edgardmessias <edgardmessias@gmail.com>
2013-02-26 14:22:02 -0300

    Added support for findTableNames in Schema;

--- CIbmDB2Schema.php
+++ CIbmDB2Schema.php
@@ -24,4 +24,32 @@ class CIbmDB2Schema extends CDbSchema {
         
     }
 
+    /**
+     * Returns all table names in the database.
+     * @param string $schema the schema of the tables. Defaults to empty string, meaning the current or default schema.
+     * If not empty, the returned table names will be prefixed with the schema name.
+     * @return array all table names in the database.
+     */
+    protected function findTableNames($schema = '') {
+        $sql = <<<EOD
+SELECT syscat.tables.tabname
+FROM syscat.tables
+WHERE syscat.type IN ('T', 'V')
+EOD;
+        if ($schema !== '') {
+            $sql .= <<<EOD
+AND   syscat.tables.tabschema=:schema
+EOD;
+        }
+        $sql .= <<<EOD
+ORDER BY syscat.tables.tabname;
+EOD;
+        $command = $this->getDbConnection()->createCommand($sql);
+        if ($schema !== '') {
+            $command->bindParam(':schema', $schema);
+        }
+        return $command->queryColumn();
+        ;
+    }
+
 }
