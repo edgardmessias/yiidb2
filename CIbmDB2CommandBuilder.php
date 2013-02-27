b886f8721c9bd6a8216dbe4ba842956792112c3c
edgardmessias <edgardmessias@gmail.com>
2013-02-27 10:19:37 -0300

    Implemented method applyLimit in CommandBuilder;

--- CIbmDB2CommandBuilder.php
+++ CIbmDB2CommandBuilder.php
@@ -14,5 +14,33 @@
  * @package ext.yiidb2
  */
 class CIbmDB2CommandBuilder extends CDbCommandBuilder {
-    
+
+    /**
+     * Alters the SQL to apply LIMIT and OFFSET.
+     * Default implementation is applicable for PostgreSQL, MySQL and SQLite.
+     * @param string $sql SQL query string without LIMIT and OFFSET.
+     * @param integer $limit maximum number of rows, -1 to ignore limit.
+     * @param integer $offset row offset, -1 to ignore offset.
+     * @return string SQL with LIMIT and OFFSET
+     */
+    public function applyLimit($sql, $limit, $offset) {
+        $limit = $limit !== null ? (int) $limit : 0;
+        $offset = $offset !== null ? (int) $offset : 0;
+
+        if ($limit > 0 && $offset <= 0) {
+            $sql.=' FETCH FIRST ' . $limit . 'ROWS ONLY';
+        } elseif ($offset > 0) {
+            $query = 'SELECT dbnumberedrows.* FROM (SELECT ROW_NUMBER() OVER() AS dbrownumber, dbresult.* FROM (' . $sql . ') AS dbresult)';
+            if ($limit == 1) {
+                $query .= ' WHERE (dbnumberedrows.dbrownumber = ' . ($offset + 1) . ')';
+            } elseif ($limit > 0) {
+                $query .= ' WHERE (dbnumberedrows.dbrownumber BETWEEN ' . ($offset + 1) . ' AND ' . ($offset + $limit) . ')';
+            } else {
+                $query .= ' WHERE (dbnumberedrows.dbrownumber > ' . ($offset + 1) . ')';
+            }
+            return $query;
+        }
+        return $sql;
+    }
+
 }
